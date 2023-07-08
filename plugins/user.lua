396bb2583a4bf9d30584301108c189b4e7e79bbc
Joseph Hansen <Joseph.hansen.96@gmail.com>
2023-07-08 00:33:56 -0700

    added catppuccin theme and everforest

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,6 +2,26 @@ return {
   {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.catppuccin" },
+    { -- further customize the options set by the community
+      "catppuccin",
+      opts = {
+        flavour = "frappe",
+        integrations = {
+          sandwich = false,
+          noice = true,
+          mini = true,
+          leap = true,
+          markdown = true,
+          neotest = true,
+          cmp = true,
+          overseer = true,
+          lsp_trouble = true,
+          ts_rainbow2 = true,
+        },
+      },
+    },
     { import = "astrocommunity.colorscheme.gruvbox-baby" },
+    { import = "astrocommunity.colorscheme.nord-nvim" },
+    { import = "astrocommunity.colorscheme.everforest" },
   },
 }
