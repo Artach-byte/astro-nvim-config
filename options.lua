1d4f1238d22eaf9f6fe9bee7c3b5d5f1b3498ab4
Joseph Hansen <56806498+Artach-byte@users.noreply.github.com>
2023-07-12 02:33:27 -0700

    disabled swapfile

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     cmdheight=1, -- renables cmd line
+    swapfile=false, -- disables swapfile
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
